674ec045d6876f92e60e4fec0efd66983c623306
Sumanth Ratna <sumanth.ratna@yale.edu>
2025-05-05 18:15:44 -0400

    Update run_benchmark to support different graphs

--- run_benchmark.sh
+++ run_benchmark.sh
@@ -9,5 +9,21 @@ EXEC=$1
 GRAPH=$2
 THRESH=$3
 
-echo "Running ./$EXEC $GRAPH 916428 $THRESH 0.85 8"
-./$EXEC $GRAPH 916428 $THRESH 0.85 8
+# Get number of vertices based on graph name
+# TODO: node labels for gplus_combined, twitter_combined
+# TODO: final error=inf and ranks 0 for web-BerkStan
+case "$GRAPH" in
+    *"amazon0601"*) V=403394 ;;
+    *"gplus_combined"*) V=107614 ;;
+    *"soc-pokec-relationships"*) V=1632804 ;;
+    *"twitter_combined"*) V=81306 ;;
+    *"web-BerkStan"*) V=685230 ;;
+    *"web-Google"*) V=916428 ;;
+    *"web-NotreDame"*) V=325729 ;;
+    *"web-Stanford"*) V=281904 ;;
+    *"wiki-Talk"*) V=2394385 ;;
+    *) echo "Unknown graph: $GRAPH"; exit 1 ;;
+esac
+
+echo "Running ./$EXEC $GRAPH $V $THRESH 0.85 8"
+./$EXEC $GRAPH $V $THRESH 0.85 8
